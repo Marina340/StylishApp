37b37bf23e69e8a7881fc7fa56d6e1afdb0fe3b0
Sara Waleed <sarawaleedmohamed2@gmail.com>
2025-05-13 15:52:23 +0300

    sidebar

--- app/src/main/java/com/example/stylish/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/stylish/ui/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.stylish.presentation.pages
 import BannerSection
 import CustomTopBar
+import SidebarUI
 import com.example.stylish.presentation.widget.SearchBar
 import com.example.stylish.presentation.widget.FeaturedSection
 import com.example.stylish.presentation.widget.CategoryList
@@ -18,73 +19,93 @@ import com.example.stylish.presentation.widget.ProductGridd
 import com.example.stylish.presentation.widget.ProductHorizontalList
 import com.example.stylish.presentation.widget.Screen
 import com.example.stylish.ui.screens.CategoryRow
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+
 //import com.example.stylish.ui.screens.GroupSelectionScreen
-
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
-    Scaffold(
-        topBar = {
-            CustomTopBar(navController)
-        }
-    ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding), // Ensure content doesn't overlap with AppBar
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        ) {
-           item {
-               Column {
-                   Text(
-                       modifier = Modifier.padding(start = 16.dp, top = 8.dp),
-                       text = "Categories",
-                       fontWeight = FontWeight.Bold,
-                       fontSize = 18.sp
-                   )
-                   CategoryRow(
-                       onCategoryClick = { categorySlug ->
-                           navController.navigate("products/$categorySlug")
-                       }
-                   )
-               }
-           }
-            item { BannerSection() }
-            item {
-                Text(
-                    modifier = Modifier.padding(start = 16.dp),
-                    text = "Best Seller",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-                )
-            }
-            item {
-                ProductHorizontalList(onProductClick = { product ->
-                    navController.currentBackStackEntry
-                        ?.savedStateHandle
-                        ?.set("product", product)
-                    navController.navigate(Screen.ProductDetail.route)
-                })
-            }
-            item { DealsSection() }
-            item {
-                Text(
-                    modifier = Modifier.padding(start = 16.dp),
-                    text = "Products",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp
-                )
-            }
-            item {   ProductGridd(
-                onProductClick = { product ->
-                    navController.currentBackStackEntry
-                        ?.savedStateHandle
-                        ?.set("product", product)
-                    navController.navigate(Screen.ProductDetail.route)
-                }
-            )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+           SidebarUI()
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                CustomTopBar(
+                    navController = navController,
+                    onMenuClick = {
+                        scope.launch {
+                            drawerState.open()
                         }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                item {
+                    Column {
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp, top = 8.dp),
+                            text = "Categories",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        CategoryRow(
+                            onCategoryClick = { categorySlug ->
+                                navController.navigate("products/$categorySlug")
+                            }
+                        )
+                    }
+                }
+                item { BannerSection() }
+                item {
+                    Text(
+                        modifier = Modifier.padding(start = 16.dp),
+                        text = "Best Seller",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                    )
+                }
+                item {
+                    ProductHorizontalList(onProductClick = { product ->
+                        navController.currentBackStackEntry
+                            ?.savedStateHandle
+                            ?.set("product", product)
+                        navController.navigate(Screen.ProductDetail.route)
+                    })
+                }
+                item { DealsSection() }
+                item {
+                    Text(
+                        modifier = Modifier.padding(start = 16.dp),
+                        text = "Products",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                }
+                item {
+                    ProductGridd(
+                        onProductClick = { product ->
+                            navController.currentBackStackEntry
+                                ?.savedStateHandle
+                                ?.set("product", product)
+                            navController.navigate(Screen.ProductDetail.route)
+                        }
+                    )
+                }
+            }
         }
     }
 }
+
