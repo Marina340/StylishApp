561ef33a980978ae2139262677db12398c63b833
mariam-mohamed-1 <128905232+mariam-mohamed-1@users.noreply.github.com>
2025-05-14 20:26:00 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
@@ -10,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.stylish"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -40,11 +41,32 @@ android {
 }
 
 dependencies {
+
+
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.foundation:foundation:1.4.3")
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.7.8")
     implementation ("androidx.compose.material:material-icons-extended:1.5.0")
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("androidx.biometric:biometric:1.1.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // مكتبة لاضافة البصمة
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Coroutines
+    implementation (
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3"
+    )
+
+    // Compose
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.5.4")
+
+    // Coil (Image Loading)
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -60,6 +82,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-
+    implementation ("com.google.android.gms:play-services-base:18.4.0")
 }
