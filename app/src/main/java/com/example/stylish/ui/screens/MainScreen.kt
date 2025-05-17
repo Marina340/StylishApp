080c1ea1b05c2fa7c5a79b5a04fe9f7d0393b838
CASHCALL\mehab <marina.ehab@cashcall.com.eg>
2025-05-17 14:03:29 +0300

    merge mariam

--- app/src/main/java/com/example/stylish/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/stylish/ui/screens/MainScreen.kt
@@ -6,11 +6,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.settingscreen.SettingScreens.SettingsScreen
 import com.example.stylish.data.Models.LoginResponse
 import com.example.stylish.presentation.components.BottomNavBar
 import com.example.stylish.ui.components.LoginComponents.PrefsManager
 import com.example.stylish.ui.screens.WishlistPage
-import com.example.stylish.ui.components.LoginComponents.PrefsManager
 @Composable
 fun MainScreen(navController: NavController,prefs: PrefsManager , user :LoginResponse?) {
 //    val navController = rememberNavController()
